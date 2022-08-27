55a7f79f3f972e3f2fa4cc82d9382355921a225b
VWL <victor.w.lee@outlook.com>
2022-08-27 15:29:44 +1200

    added new .c files

---
+++ c/freeCodeCampTuts/littleGame.c
@@ -0,0 +1,10 @@
+/* Make a little text based game */
+
+#include <stdio.h>
+
+int main() {
+    int orderList[20] = {1, 3, 5, 7, 9, 10};
+    printf("%d, %d, %d, %d, %d\n", orderList[1], orderList[2], orderList[3], orderList[4], orderList[0]);
+    return 0;
+
+}
