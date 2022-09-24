1b57ed133b7f4029f3cb2b8a8075562af8ddb89d
VW <victor.w.lee@outlook.com>
2022-09-24 16:05:59 +1200

    modified lua script

--- lua/love2d_edx/main.lua
+++ lua/love2d_edx/main.lua
@@ -1,14 +1,14 @@
 function love.load()
-    number = 0
 end
 
 
-function love.update(dt)
-    number = number + 1
+function love.update()
 end
 
 function love.draw()
-    love.graphics.rectangle("fill", 0, 0, 200, 100)
-    love.graphics.rectangle("fill", 800, 800, 200, 100)
+    love.graphics.rectangle("fill", 200, 200, 50, 50)
+    love.graphics.circle("line", 200, 200, 2.3)
+    love.graphics.setColor(255/255, 1/1, 2/2)
+
 
 end
