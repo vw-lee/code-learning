082e3097fea16d17c494d82b99d7dfed96796051
VW <victor.w.lee@outlook.com>
2022-09-24 16:09:25 +1200

    modified lua script

--- lua/love2d_edx/main.lua
+++ lua/love2d_edx/main.lua
@@ -1,13 +1,19 @@
 function love.load()
+    target = {}
+    target.x = 300
+    target.y = 300
+    target.radius = 50
+
+    score = 0
+    timer = 0
 end
 
-
-function love.update()
+function love.update(dt)
 end
 
 function love.draw()
     love.graphics.rectangle("fill", 200, 200, 50, 50)
-    love.graphics.circle("line", 200, 200, 2.3)
+    love.graphics.circle("line", target.x, target.y, target.radius)
     love.graphics.setColor(255/255, 1/1, 2/2)
 
 
