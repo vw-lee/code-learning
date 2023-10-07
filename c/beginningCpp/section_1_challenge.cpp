95c8496fe8e2375be5cadb18a1b8b49c0204f6d8
victor.lee <victor.lee@roboticsplus.co.nz>
2023-10-08 11:41:46 +1300

    added section_1_challenge.cpp and finished section 1.

--- c/beginningCpp/section_1_challenge.cpp
+++ c/beginningCpp/section_1_challenge.cpp
@@ -2,10 +2,11 @@
 
 int main() {
     int fav_num;
-    std::cout << "Enter your favourite number between 1 to 100:";
+    std::cout << "Enter your favourite number between 1 to 100: ";
     std::cin >> fav_num;
 
     std::cout << "Amazing, that's my favourite number too!" << std::endl;
+    std::cout << "No really!!" << fav_num << " is my favourite number!" << std::endl;
 
     return 0;
 }
