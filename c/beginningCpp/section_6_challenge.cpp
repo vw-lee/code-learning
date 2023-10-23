7e0c43efe69eaf00c19e1353a87f4cec54be2617
rpl-victor-lee <victor.lee@roboticsplus.co.nz>
2023-10-23 20:04:46 +1300

    added section 6 challenge in c

---
+++ c/beginningCpp/section_6_challenge.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+
+// global variables can be accessed anywhere in the program
+int global = 0;
+
+int main () {
+    unsigned int a = -25;
+    int b = -25;
+    cout << a + b << endl;
+    return 0;
+}
+
