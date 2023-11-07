bde3d0e6591b0a216f26e3410e80a40bb2a3d554
vw-lee <victor.w.lee@outlook.com>
2023-11-07 21:16:29 +1300

    updated section 8

--- c/beginningCpp/section_8.cpp
+++ c/beginningCpp/section_8.cpp
@@ -1,3 +1,50 @@
+// expressions and statements
+
 #include <iostream>
 #include <vector>
+
+using namespace std;
+
+int main() { 
+    
+    double num1 {10}; 
+    double num2 {5}; 
+
+    vector <double> result (5,0);
+    result[0] = num1 * num2;
+    result[1] = num1 / num2;
+    result[2] = int(num1) % int(num2);
+    result[3] = num1 + num2;
+
+    cout << "the result is " 
+    << endl
+    << result[0] 
+    << endl
+    << result[1] 
+    << endl
+    << result[2] 
+    << endl
+    << result[3] 
+    << endl;
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
